e75d4aa181b5e8500d686c53c44386136f081233
AnarTechJourney <anartalibov477@gmail.com>
2024-07-23 17:57:53 +0400

    fixing issue
    fixing scroll to top issue

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Main from "./Main";
+import ScrollToTop from "../utils/ScrollToTop";
 
 const AppLayout = () => {
   return (
     <div>
+      <ScrollToTop />
       <Header />
       <Main />
     </div>
